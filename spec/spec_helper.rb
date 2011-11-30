2afdc98748ff3238b252768691bc8f4a01f69914
Bodaniel Jeanes <me@bjeanes.com>
2011-11-29 20:20:14 -0600

    Remove unnecessary require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
-require 'json'
 require 'fixture_helper'
 require 'pair'
 
