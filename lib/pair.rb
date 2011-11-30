e0ea4f28567e13a6ce3d5e397a9a2ba625fdf5e2
Bodaniel Jeanes <me@bjeanes.com>
2011-11-29 22:27:16 -0600

    cleanup

--- lib/pair.rb
+++ lib/pair.rb
@@ -1,10 +1,8 @@
 require 'httparty'
-require 'ruby-debug' if ENV["DEBUGGER"] == "true"
 
 require "pair/version"
 require "pair/api"
 require "pair/session"
 
 module Pair
-  # Your code goes here...
 end
